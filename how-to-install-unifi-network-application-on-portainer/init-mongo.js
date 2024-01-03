6cca14ec5d1a0edae8d9141ae4857576dfe856cb
Christopher <github.bigbeartechworld@emailcove.com>
2024-01-03 14:22:07 -0600

    Update init-mongo.js to assign dbOwner role for unifi_stat database

--- how-to-install-unifi-network-application-on-portainer/init-mongo.js
+++ how-to-install-unifi-network-application-on-portainer/init-mongo.js
@@ -1,8 +1,9 @@
+// Connect to the "unifi" database and create a user
 db.getSiblingDB("unifi").createUser({
   user: "unifi",
   pwd: "unifi_password", // Replace with your actual password
   roles: [
-    { role: "dbOwner", db: "unifi" },
-    { role: "dbOwner", db: "unifi_stat" }, // Adjust if you need this additional database
+    { role: "dbOwner", db: "unifi" }, // Assign the "dbOwner" role for the "unifi" database
+    { role: "dbOwner", db: "unifi_stat" }, // Assign the "dbOwner" role for the "unifi_stat" database
   ],
 });
