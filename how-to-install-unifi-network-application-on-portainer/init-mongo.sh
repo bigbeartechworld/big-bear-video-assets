c88a75c65a7b41e023ab5256412c0dd885cfd8a3
Christopher <github.bigbeartechworld@emailcove.com>
2024-08-06 21:44:28 -0500

    chore: update init-mongo.sh script for unifi installation

--- how-to-install-unifi-network-application-on-portainer/init-mongo.sh
+++ how-to-install-unifi-network-application-on-portainer/init-mongo.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 mongo <<EOF
-use admin
-db.auth("unifi", "unifi_password")
 use unifi
 db.createUser({
-  user: "${MONGO_USER}",
-  pwd: "${MONGO_PASS}",
+  user: "unifi",
+  pwd: "unifi_password",
   roles: [
     { db: "unifi", role: "dbOwner" },
     { db: "unifi_stat", role: "dbOwner" }
